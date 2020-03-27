d839322918b54898e55479240f83dd3119eda10b
Ali Sharifineyestani <sh4rifi@gmail.com>
2020-03-27 20:55:03 +0430

    add StopWatch

---
+++ src/components/StopWatch/StopWatch.js
@@ -0,0 +1,33 @@
+import React, {useState} from 'react';
+
+const style = {
+    timer: {
+        fontSize: 36
+    }
+}
+
+function StopWatch() {
+
+    const [elapsed, setElapsed] = useState(0)
+
+    const getMillisecond = () => {
+        return ('0' + elapsed * 100).slice(-2);
+    }
+    const getSecond = () => {
+        return ('0' + elapsed % 60).slice(-2);
+    }
+    const getMinute = () => {
+        return ('0' + Math.floor(elapsed / 60)).slice(-2);
+    }
+
+
+    return (
+        <div className="display">
+            <div style={style.timer}>{getMinute()}:{getSecond()}:{getMillisecond()}</div>
+        </div>
+    );
+}
+
+
+export default StopWatch;
+
